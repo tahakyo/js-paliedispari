6772c8f3f271f7f5b67c29f75bfe16dc9f75634d
tahakyo <ghazouani1988@email.com>
2022-04-21 19:43:36 +0200

    add function invertWord

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,19 +4,32 @@
 // chideo all'utente di inserire una parola
 const userWord = prompt("inserisci una parola");
 
+const invertedWord = invertWord(userWord);
 
-// tasforma la parola inserita dall'utente in una array
-let splittedWord = userWord.split('');
-console.log(splittedWord)
-// inverto l'array per poter leggere la parola al contrario
-let reversedWord = splittedWord.reverse();
-console.log(splittedWord);
-// riunisco la parola in una stringa unica
- reversedWord = reversedWord.join('');
-console.log(reversedWord);
+// // tasforma la parola inserita dall'utente in una array
+// let splittedWord = userWord.split('');
+// console.log(splittedWord)
+// // inverto l'array per poter leggere la parola al contrario
+// let reversedWord = splittedWord.reverse();
+// console.log(splittedWord);
+// // riunisco la parola in una stringa unica
+//  reversedWord = reversedWord.join('');
+// console.log(reversedWord);
+
+
+
 // faccio il confronto tra la parola inserita e la parola invertita per vedere se è palindroma
-if (userWord === reversedWord) {
+if (userWord === invertedWord) {
     console.log('la parola è palindroma');
 }else {
     console.log('la parola non è palindroma');
+}
+
+
+
+
+
+function invertWord(str){
+    const strInverted = str.split('').reverse().join('');
+    return strInverted;
 }

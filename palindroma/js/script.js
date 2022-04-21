56362e1cbe14937b9565eef5a55cb7c06d57df0b
tahakyo <ghazouani1988@email.com>
2022-04-21 19:28:37 +0200

    add palindroma

---
+++ palindroma/js/script.js
@@ -0,0 +1,22 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chideo all'utente di inserire una parola
+const userWord = prompt("inserisci una parola");
+
+
+// tasforma la parola inserita dall'utente in una array
+let splittedWord = userWord.split('');
+console.log(splittedWord)
+// inverto l'array per poter leggere la parola al contrario
+let reversedWord = splittedWord.reverse();
+console.log(splittedWord);
+// riunisco la parola in una stringa unica
+ reversedWord = reversedWord.join('');
+console.log(reversedWord);
+// faccio il confronto tra la parola inserita e la parola invertita per vedere se è palindroma
+if (userWord === reversedWord) {
+    console.log('la parola è palindroma');
+}else {
+    console.log('la parola non è palindroma');
+}
